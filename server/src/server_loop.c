1e3c6f621df740ead313f7a84b546865c66ae7b2
Broke Machine <emile.ettel@epitech.eu>
2022-05-27 15:24:40 +0200

    [STL] Quick style fix

--- server/src/server_loop.c
+++ server/src/server_loop.c
@@ -17,12 +17,26 @@ int client_list_count(my_client_t *clients)
     return count;
 }
 
+void get_message(my_server_t *serv, int i)
+{
+    int nread;
+    char *buffer;
+
+    ioctl(i, FIONREAD, &nread);
+    if (nread == 0) {
+        del_client(serv, i);
+        FD_CLR(i, &serv->fds);
+        return;
+    }
+    buffer = get_client_line(i);
+    printf("%s\n", buffer);
+    free(buffer);
+}
+
 void incoming_message(my_server_t *serv, int i)
 {
     struct sockaddr_in client;
     int cli_fd;
-    char *buffer;
-    int nread;
 
     if (FD_ISSET(i, &serv->tmp_fds)) {
         if (i == serv->server_fd) {
@@ -31,17 +45,9 @@ void incoming_message(my_server_t *serv, int i)
             FD_SET(cli_fd, &serv->fds);
             add_client(serv, make_client(cli_fd, serv->width,
             serv->height));
-        } else {
-            ioctl(i, FIONREAD, &nread);
-            if (nread == 0) {
-                del_client(serv, i);
-                FD_CLR(i, &serv->fds);
-                return;
-            }
-            buffer = get_client_line(i);
-            printf("%s\n", buffer);
-            free(buffer);
+            return;
         }
+        get_message(serv, i);
     }
 }
 
@@ -49,7 +55,7 @@ void server_loop(my_server_t *serv)
 {
     while (1) {
         serv->tmp_fds = serv->fds;
-        select(FD_SETSIZE, &serv->tmp_fds, NULL, NULL, NULL);
+        select(FD_SETSIZE, &serv->tmp_fds, &serv->tmp_fds, NULL, NULL);
         for (int i = 0; i < FD_SETSIZE; i++)
             incoming_message(serv, i);
         printf("Connected clients: %d\n", client_list_count(serv->clients));
