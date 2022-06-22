400443cc4e56439964fb56d7dce7b0076d86c4ae
NGUYEN Anthony <anthony1.nguyen@epitech.eu>
2022-06-22 11:03:04 +0200

    [ADD] Player count check on ritual

--- server/src/check_ritual.c
+++ server/src/check_ritual.c
@@ -20,6 +20,8 @@ void ritual_proceed(my_client_t *client, inv_t ritual)
 
 void check_rit_inv(my_client_t *client, inv_t ritual)
 {
+    if (client->inventory->player < ritual.player)
+        return;
     if (client->inventory->linemate < ritual.linemate)
         return;
     if (client->inventory->deraumere < ritual.deraumere)
@@ -40,15 +42,15 @@ void check_ritual_high_level(my_client_t *client)
     switch (client->level) {
     case 5:
         check_rit_inv(client, (inv_t)
-        {.linemate=1, .deraumere=2, .sibur=1, .mendiane=3});
+        {.player=4, .linemate=1, .deraumere=2, .sibur=1, .mendiane=3});
         break;
     case 6:
         check_rit_inv(client, (inv_t)
-        {.linemate=1, .deraumere=2, .sibur=3, .phiras=1});
+        {.player=6, .linemate=1, .deraumere=2, .sibur=3, .phiras=1});
         break;
     case 7:
         check_rit_inv(client, (inv_t)
-        {.linemate=2, .deraumere=2, .sibur=2,
+        {.player=6, .linemate=2, .deraumere=2, .sibur=2,
             .mendiane=2, .phiras=2, .thystame=1});
         break;
     default:
@@ -60,17 +62,19 @@ void check_ritual_level(my_client_t *client)
 {
     switch (client->level) {
     case 1:
-        check_rit_inv(client, (inv_t){.linemate=1});
+        check_rit_inv(client, (inv_t){.player=1, .linemate=1});
         break;
     case 2:
-        check_rit_inv(client, (inv_t){.linemate=1, .deraumere=1, .sibur=1});
+        check_rit_inv(client, (inv_t)
+        {.player=2, .linemate=1, .deraumere=1, .sibur=1});
         break;
     case 3:
-        check_rit_inv(client, (inv_t){.linemate=2, .sibur=1, .phiras=2});
+        check_rit_inv(client, (inv_t)
+        {.player=2, .linemate=2, .sibur=1, .phiras=2});
         break;
     case 4:
         check_rit_inv(client, (inv_t)
-        {.linemate=1, .deraumere=1, .sibur=2, .phiras=1});
+        {.player=4, .linemate=1, .deraumere=1, .sibur=2, .phiras=1});
         break;
     default:
         check_ritual_high_level(client);
