2b5b6453306badcc1aa25d17c6a4e9d6c0f23ba2
NGUYEN Anthony <anthony1.nguyen@epitech.eu>
2022-06-24 02:14:23 +0200

    [FIX] Norme

--- server/src/connect_gui.c
+++ server/src/connect_gui.c
@@ -7,15 +7,6 @@
 
 #include "server.h"
 
-void print_arr(char **arr)
-{
-    puts("Printing Arr :");
-    for (int i = 0; arr[i]; i++) {
-        puts(arr[i]);
-    }
-    puts("END");
-}
-
 bool my_strcmp(char *s1, char *s2)
 {
     if (!s1 || !s2)
@@ -44,29 +35,37 @@ bool is_gui_message(char **arr)
     return true;
 }
 
+void gui_server_catchup_send(my_server_t *s, int i, int j)
+{
+    inv_t **m = s->map;
+
+    if (m[i][j].linemate > 0)
+        dprintf(s->gui_fd, "l %i %c %c\n", m[i][j].linemate, j + 'a', i + 'a');
+    if (m[i][j].deraumere > 0)
+        dprintf(s->gui_fd, "d %i %c %c\n", m[i][j].deraumere, j + 'a', i + 'a');
+    if (m[i][j].sibur > 0)
+        dprintf(s->gui_fd, "s %i %c %c\n", m[i][j].sibur, j + 'a', i + 'a');
+    if (m[i][j].mendiane > 0)
+        dprintf(s->gui_fd, "m %i %c %c\n", m[i][j].mendiane, j + 'a', i + 'a');
+    if (m[i][j].phiras > 0)
+        dprintf(s->gui_fd, "p %i %c %c\n", m[i][j].phiras, j + 'a', i + 'a');
+    if (m[i][j].thystame > 0)
+        dprintf(s->gui_fd, "t %i %c %c\n", m[i][j].thystame, j + 'a', i + 'a');
+    if (m[i][j].food > 0)
+        dprintf(s->gui_fd, "f %i %c %c\n", m[i][j].food, j + 'a', i + 'a');
+}
+
 void gui_server_catchup(my_server_t *s)
 {
     my_client_t *c = s->clients;
     while (c) {
-        dprintf(s->gui_fd, "player new %i %i %i %s %s", c->direction, c->x, c->y, c->team_name, c->name);
+        dprintf(s->gui_fd, "player new %i %i %i %s %s",
+        c->direction, c->x, c->y, c->team_name, c->name);
         c = c->next;
     }
     for (uint i = 0; i < s->height; i++) {
         for (uint j = 0; j < s->width; j++) {
-            if (s->map[i][j].linemate > 0)
-                dprintf(s->gui_fd, "l %i %c %c\n", s->map[i][j].linemate, j + 'a', i + 'a');
-            if (s->map[i][j].deraumere > 0)
-                dprintf(s->gui_fd, "d %i %c %c\n", s->map[i][j].deraumere, j + 'a', i + 'a');
-            if (s->map[i][j].sibur > 0)
-                dprintf(s->gui_fd, "s %i %c %c\n", s->map[i][j].sibur, j + 'a', i + 'a');
-            if (s->map[i][j].mendiane > 0)
-                dprintf(s->gui_fd, "m %i %c %c\n", s->map[i][j].mendiane, j + 'a', i + 'a');
-            if (s->map[i][j].phiras > 0)
-                dprintf(s->gui_fd, "p %i %c %c\n", s->map[i][j].phiras, j + 'a', i + 'a');
-            if (s->map[i][j].thystame > 0)
-                dprintf(s->gui_fd, "t %i %c %c\n", s->map[i][j].thystame, j + 'a', i + 'a');
-            if (s->map[i][j].food > 0)
-                dprintf(s->gui_fd, "f %i %c %c\n", s->map[i][j].food, j + 'a', i + 'a');
+            gui_server_catchup_send(s, i, j);
         }
     }
 }
